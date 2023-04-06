04a5af9beee5c6e40f35a46baebe14a2cd08f903
Nick Kudryavtsev <ch33rnomore@gmail.com>
2023-04-06 21:54:00 +0300

    Fix 30_brain_calc_spec line 25 fail #2

--- app/src/main/java/hexlet/code/games/GCD.java
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -8,11 +8,11 @@ public class GCD {
         Object[][] questionAnswerIteration = new Object[questionAnswersCount][2];
 
         for (int i = 0; i < questionAnswersCount; i++) {
-            int num1 = generateRandomOperand();
-            int num2 = generateRandomOperand();
+            int number1 = generateRandomOperand();
+            int number2 = generateRandomOperand();
 
-            String question = generateQuestion(num1, num2);
-            String answer = generateAnswer(num1, num2);
+            String question = generateQuestion(number1, number2);
+            String answer = generateAnswer(number1, number2);
             questionAnswerIteration[i][0] = question;
             questionAnswerIteration[i][1] = answer;
         }
@@ -20,31 +20,31 @@ public class GCD {
         return questionAnswerIteration;
     }
 
-    public static String generateQuestion(int num1, int num2) {
-        return num1 + " " + num2;
+    public static String generateQuestion(int number1, int number2) {
+        return number1 + " " + number2;
     }
 
-    public static String generateAnswer(int num1, int num2) {
-        return Integer.toString(getGCD(num1, num2));
+    public static String generateAnswer(int number1, int number2) {
+        return Integer.toString(getGCD(number1, number2));
     }
 
     public static int generateRandomOperand() {
         return RandomUtils.nextInt(0, 100);
     }
 
-    public static int getGCD(int num1, int num2) {
-        if (num1 == num2) {
-            return num1;
+    public static int getGCD(int number1, int number2) {
+        if (number1 == number2) {
+            return number1;
         }
 
-        while (num1 != num2) {
-            if (num1 > num2) {
-                num1 = num1 - num2;
+        while (number1 != number2) {
+            if (number1 > number2) {
+                number1 = number1 - number2;
             } else {
-                num2 = num2 - num1;
+                number2 = number2 - number1;
             }
         }
 
-        return num1;
+        return number1;
     }
 }
