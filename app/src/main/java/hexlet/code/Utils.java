73c67c5ff74f402b9587b09d38a1be9f6d53fb27
ch33rnomore <ch33rnomore@gmail.com>
2024-02-23 18:35:03 +0300

    Last commit before checking: fix checkstyle errors

--- app/src/main/java/hexlet/code/Utils.java
+++ app/src/main/java/hexlet/code/Utils.java
@@ -1,7 +1,5 @@
 package hexlet.code;
 
-import java.util.Scanner;
-
 public class Utils {
     public static int generateRandomInt(int min, int max) {
         return (int) ((Math.random() * (max - min)) + min);
