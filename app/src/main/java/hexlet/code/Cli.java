7ce13245b870cc024279c22be1aa2913a42e38b7
ch33rnomore <lautrezx@macbook-air-angelina.local>
2024-05-06 20:03:01 +0300

    Fix last teacher remarks - fix asciinema - fix greetings

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,15 +1,7 @@
 package hexlet.code;
 
-import java.util.Scanner;
-
 public class Cli {
     public static void greetings() {
-        String username;
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("May I have your name? ");
-        username = scanner.nextLine();
-
-        System.out.println("Hello, " + username + "!");
+        System.out.println("Welcome to the Brain Games!");
     }
 }
--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -28,7 +28,7 @@ public class Progression {
     }
 
     public static void run() {
-        String[][] gameData = generateGameData(5);
+        String[][] gameData = generateGameData(3);
         Engine.launchGame(gameData, GAME_RULES);
     }
 
