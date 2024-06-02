aa925ac8ea7ee8d0800fa52dfca7e934e5777da0
ch33rnomore <ch33rnomore@gmail.com>
2024-06-02 18:12:19 +0300

    Changed static sequences to dynamic. Fix quota replace to join

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -7,41 +7,24 @@ import static hexlet.code.Engine.ROUNDS;
 
 public class Calc {
     private static final String GAME_RULES = "What is the result of the expression?";
-    private static final double PLUS_CONDITION_LIMIT = .2;
-    private static final double MINUS_CONDITION_LIMIT = .3;
     private static final int MIN_GENERATED_VALUE = 1;
     private static final int MAX_GENERATED_VALUE = 100;
-//    private static final int QUESTIONS_COUNT = 3;
+    private static final String[] OPERATORS = {"+", "-", "*"};
 
     public static String[][] generateGameData(int questionsCount) {
         String[] questions = new String[questionsCount];
         String[] answers = new String[questionsCount];
-        String[][] questionsAnswers = new String[questionsCount][questionsCount];
 
+        for (int i = 0; i < questionsCount; i++) {
+            int first = Utils.generateRandomInt(MIN_GENERATED_VALUE, MAX_GENERATED_VALUE);
+            int second = Utils.generateRandomInt(MIN_GENERATED_VALUE, MAX_GENERATED_VALUE);
+            String operator = OPERATORS[Utils.generateRandomInt(0, OPERATORS.length - 1)];
 
-        double conditions = Math.random();
-        int first;
-        int second;
-
-        for (int i = 0; i < questions.length; i++) {
-            if (conditions < PLUS_CONDITION_LIMIT) {
-                first = Utils.generateRandomInt(MIN_GENERATED_VALUE, MAX_GENERATED_VALUE);
-                second = Utils.generateRandomInt(MIN_GENERATED_VALUE, MAX_GENERATED_VALUE);
-                questions[i] = first + " - " + second;
-                answers[i] = calculate(first, second, "+");
-            } else if (conditions > PLUS_CONDITION_LIMIT && conditions < MINUS_CONDITION_LIMIT) {
-                first = Utils.generateRandomInt(MIN_GENERATED_VALUE, MAX_GENERATED_VALUE);
-                second = Utils.generateRandomInt(MIN_GENERATED_VALUE, MAX_GENERATED_VALUE);
-                questions[i] = first + " - " + second;
-                answers[i] = calculate(first, second, "-");
-            } else {
-                first = Utils.generateRandomInt(MIN_GENERATED_VALUE, MAX_GENERATED_VALUE);
-                second = Utils.generateRandomInt(MIN_GENERATED_VALUE, MAX_GENERATED_VALUE);
-                questions[i] = first + " * " + second;
-                answers[i] = calculate(first, second, "*");
-            }
+            questions[i] = first + " " + operator + " " + second;
+            answers[i] = calculate(first, second, operator);
         }
 
+        String[][] questionsAnswers = new String[2][];
         questionsAnswers[0] = questions;
         questionsAnswers[1] = answers;
 
--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -7,28 +7,30 @@ import static hexlet.code.Engine.ROUNDS;
 
 public class Progression {
     private static final String GAME_RULES = "What number is missing in the progression?";
-    private static final int INIT_VALUE = 100;
-    private static final int STEP = 100;
-    private static final int SIZE = 100;
+    private static final int MIN_INITIAL_VALUE = 0;
+    private static final int MAX_INITIAL_VALUE = 50;
+    private static final int MIN_STEP = 1;
+    private static final int MAX_STEP = 10;
+    private static final int SIZE = 10;
     private static final int MIN_GENERATE_NUM = 0;
-    private static final int MAX_GENERATE_NUM = 9;
+    private static final int MAX_GENERATE_NUM = SIZE - 1;
 
     public static String[][] generateGameData(int questionsCount) {
         String[] questions = new String[questionsCount];
         String[] answers = new String[questionsCount];
-        String[][] questionsAnswers = new String[questionsCount][questionsCount];
 
         for (int i = 0; i < questions.length; i++) {
             int randomIndex = Utils.generateRandomInt(MIN_GENERATE_NUM, MAX_GENERATE_NUM);
-            String[] progression = generateProgression(INIT_VALUE, STEP, SIZE);
+            int initValue = Utils.generateRandomInt(MIN_INITIAL_VALUE, MAX_INITIAL_VALUE);
+            int step = Utils.generateRandomInt(MIN_STEP, MAX_STEP);
+            String[] progression = generateProgression(initValue, step, SIZE);
             String correctAnswer = progression[randomIndex];
             answers[i] = correctAnswer;
             progression[randomIndex] = "..";
-            questions[i] = String.join(" ", progression)
-                    .replace("[", "")
-                    .replace("]", "");
+            questions[i] = String.join(" ", progression);
         }
 
+        String[][] questionsAnswers = new String[2][];
         questionsAnswers[0] = questions;
         questionsAnswers[1] = answers;
 
@@ -43,8 +45,7 @@ public class Progression {
     public static String[] generateProgression(int initValue, int step, int size) {
         String[] progression = new String[size];
         for (int i = 0; i < size; i++) {
-            initValue += step;
-            progression[i] = Integer.toString(initValue);
+            progression[i] = Integer.toString(initValue + i * step);
         }
         return progression;
     }
